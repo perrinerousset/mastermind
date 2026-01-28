06f22ba51f930096a91907638339c617dbb36029
Victoria Blanchard <vblanchard01@pfm-ad.pfm.loc>
2026-01-28 17:05:46 +0100

    Historique avec JSON, suppression de database

--- MVC/model/historique.py
+++ MVC/model/historique.py
@@ -1,31 +1,29 @@
-from database import get_connection
+import json
+import os
 
-class GameHistory:
+class historique:
+    def __init__(self, filename="historique.json"):
+        self.filename = filename
 
-    @staticmethod
-    def save(resultat, tentatives):
-        conn = get_connection()
-        cursor = conn.cursor()
+    def sauvegarder_partie(self, gagne, score, tentatives):
+        nouvelle_entree = {
+            "victoire": gagne,
+            "score": score,
+            "tentatives": tentatives
+        }
 
-        cursor.execute("""
-        INSERT INTO historique (resultat, tentatives)
-        VALUES (?, ?)
-        """, (resultat, tentatives))
+        donnees = self.charger_historique()
+        donnees.append(nouvelle_entree)
+        with open(self.filename, "w", encoding="utf-8") as f:
+            json.dump(donnees, f, indent=4, ensure_ascii=False)
+        print("Partie sauvegardée avec succès !")
 
-        conn.commit()
-        conn.close()
-
-    @staticmethod
-    def get_all():
-        conn = get_connection()
-        cursor = conn.cursor()
-
-        cursor.execute("""
-        SELECT resultat, tentatives, played_at
-        FROM historique
-        ORDER BY played_at DESC
-        """)
-
-        rows = cursor.fetchall()
-        conn.close()
-        return rows
+    def charger_historique(self):
+        if not os.path.exists(self.filename):
+            return [] 
+        
+        try:
+            with open(self.filename, "r", encoding="utf-8") as f:
+                return json.load(f)
+        except json.JSONDecodeError:
+            return []

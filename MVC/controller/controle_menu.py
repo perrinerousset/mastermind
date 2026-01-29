fd26c080f6e6aa53076e4ffb3ffa7e14dfe2cd51
Noémie Dubouloz <ndubouloz01@pfm-ad.pfm.loc>
2026-01-29 13:36:24 +0100

    modif controleur pour assurer le save

--- MVC/controller/controle_menu.py
+++ MVC/controller/controle_menu.py
@@ -2,36 +2,42 @@ import tkinter as tk
 from model.mastermind import mastermind
 from view.vue_principale import VuePrincipale
 from view.console import VueJeu
+from view.vue_historique import VueHistorique
+
 class Controleur:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Mastermind Projet")
         self.modele = mastermind()
         self.vue_principale = VuePrincipale(self.root, self)
-        
-        # Correction : On définit l'accueil
-        self.afficher_accueil()
-        
+        self.partie_objet = None  
+        self.vue_jeu_active = None 
+        self.afficher_accueil()   
         self.root.mainloop()
         
     def afficher_accueil(self):
-        """Affiche le message de bienvenue par défaut"""
         self.vue_principale.nettoyer_zone_centrale()
-        tk.Label(self.vue_principale.zone_centrale, 
-                 text="Bienvenue dans le Mastermind !\nCliquez sur 'Jouer' pour commencer.",
-                 font=("Arial", 16), bg="white").pack(expand=True)
+        tk.Label(self.vue_principale.zone_centrale, text="Bienvenue dans le Mastermind !\nCliquez sur 'Jouer' pour commencer.",font=("Arial", 16), bg="white").pack(expand=True)
 
+
+    def nouvelle_partie(self):
+        self.vue_principale.nettoyer_zone_centrale()
+        self.modele.Combinaison_secrete()
+        id_p = 1 
+        self.partie_objet = Jeu(id_partie=id_p)
+        self.vue_jeu_active = VueJeu(self.vue_principale.zone_centrale, self.modele, self)
+        
     def afficher_jeu(self):
         self.vue_principale.nettoyer_zone_centrale()
-        # Correction : On génère la combinaison avant de lancer la vue
-        self.modele.Combinaison_secrete() 
-        VueJeu(self.vue_principale.zone_centrale, self.modele)
+        if self.vue_jeu_active:
+            self.vue_jeu_active.redessiner_sur_parent(self.vue_principale.zone_centrale)
+        else:
+            self.nouvelle_partie()
 
     def afficher_historique(self):
-        """Active la vue historique"""
         self.vue_principale.nettoyer_zone_centrale()
-        tk.Label(self.vue_principale.zone_centrale, text="Historique des parties",
-                 font=("Arial", 16), bg="white").pack(expand=True)
+        VueHistorique(self.vue_principale.zone_centrale, self.modele)
+
 
 if __name__ == "__main__":
     Controleur()

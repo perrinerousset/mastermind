6f3c732158ae0a2d8acdde8a8a3f63a659afa183
Noémie Dubouloz <ndubouloz01@pfm-ad.pfm.loc>
2026-01-29 11:32:56 +0100

    j'avais oublié de mettre en private pour le score

--- MVC/model/score.py
+++ MVC/model/score.py
@@ -1,15 +1,15 @@
 class Score:
     def __init__(self):  
-        self.valeur_score = 0
-        self.tentatives_max = 12
+        self.__valeur_score = 0
+        self.__tentatives_max = 12
     
     def calculer(self, nb_tentatives):
         #Si le joueur trouve en 1 tour : 1200 points.
         #Si le joueur trouve en 12 tours : 100 point. bref c'est 1200 -100*tentatives
-        if nb_tentatives > self.tentatives_max:
-            self.valeur_score = 0
+        if nb_tentatives > self.__tentatives_max:
+            self.__valeur_score = 0
         else:
-            self.valeur_score = ((self.tentatives_max - nb_tentatives) + 1) * 100
-        return self.valeur_score
+            self.__valeur_score = ((self.__tentatives_max - nb_tentatives) + 1) * 100
+        return self.__valeur_score
     def reinitialiser(self):
-        self.valeur_score = 0
+        self.__valeur_score = 0

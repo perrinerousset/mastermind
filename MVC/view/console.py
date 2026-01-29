1b6d8f60db5a02d06b697a892af07cc1ec5284c4
Noémie Dubouloz <ndubouloz01@pfm-ad.pfm.loc>
2026-01-29 09:35:48 +0100

    modif pour clareté

--- MVC/view/console.py
+++ MVC/view/console.py
@@ -6,32 +6,21 @@ class VueJeu:
         self.parent = parent_frame
         self.modele = modele
         self.couleurs = self.modele.ListeCouleur
-        
-        # On suit la ligne où se trouve le joueur (0 à 11)
-        self.ligne_actuelle = 0
-        
-        # État initial : 12 lignes de 4 pions
+        self.ligne_actuelle = 0# On suit la ligne où se trouve le joueur
         self.etat_pions = [[self.couleurs[0] for _ in range(4)] for _ in range(12)]
-        
-        # Stockage pour manipuler les éléments après création
         self.pions_graphiques = [] 
         self.boutons_valider = []
         self.resultats_graphiques = [] 
-        
         self.dessiner_plateau()
 
     def dessiner_plateau(self):
         self.canvas = tk.Canvas(self.parent, bg="#8B4513", width=500, height=580, highlightthickness=0)
         self.canvas.pack(pady=10)
-
         hauteur_ligne = 45
         offset_y = 20
 
         for i in range(12):
             y_ligne = offset_y + (i * hauteur_ligne)
-            
-            # --- GAUCHE : Bouton Valider ---
-            # Seul le premier bouton (ligne 0) est actif au début
             etat_initial = "normal" if i == 0 else "disabled"
             
             btn = tk.Button(self.parent, text="Valider", state=etat_initial,
@@ -39,22 +28,15 @@ class VueJeu:
             
             self.canvas.create_window(50, y_ligne + 20, window=btn)
             self.boutons_valider.append(btn) # On stocke pour changer l'état plus tard
-
-            # --- CENTRE : Les 4 pions ---
             ligne_pions_ids = []
-            for j in range(4):
+            for j in range(4):#création des pions même si c'est un peu moche
                 x_pion = 120 + (j * 50)
                 pion_id = self.canvas.create_oval(x_pion, y_ligne + 5, x_pion + 30, y_ligne + 35, 
                                                   fill=self.etat_pions[i][j], outline="white", width=2)
                 ligne_pions_ids.append(pion_id)
-                
-                # Le clic n'est actif que si c'est la ligne en cours
                 if i == 0:
                     self.canvas.tag_bind(pion_id, "<Button-1>", lambda event, l=i, p=j: self.changer_couleur(l, p))
-            
             self.pions_graphiques.append(ligne_pions_ids)
-
-            # --- DROITE : Résultats ---
             r_rouge = self.canvas.create_oval(350, y_ligne + 10, 370, y_ligne + 30, fill="red", state='hidden')
             t_rouge = self.canvas.create_text(380, y_ligne + 20, text="0", fill="white", state='hidden')
             r_blanc = self.canvas.create_oval(410, y_ligne + 10, 430, y_ligne + 30, fill="white", state='hidden')
@@ -68,8 +50,7 @@ class VueJeu:
             self.canvas.create_line(10, y_ligne + hauteur_ligne, 490, y_ligne + hauteur_ligne, fill="white")
 
     def changer_couleur(self, ligne, index_pion):
-        # On ne change la couleur que si c'est la ligne active
-        if ligne != self.ligne_actuelle:
+        if ligne != self.ligne_actuelle:# On ne change la couleur que si c'est la ligne active
             return
 
         couleur_actuelle = self.etat_pions[ligne][index_pion]

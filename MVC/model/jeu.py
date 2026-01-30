45871d694a813e9c9130016ddaa9f88106f56563
Perrine Rousset <prousset01@pfm-ad.pfm.loc>
2026-01-30 19:08:21 +0100

    gestion du bug de mise à jour historique18 jai reussi zbi

--- MVC/model/jeu.py
+++ MVC/model/jeu.py
@@ -5,37 +5,43 @@ class Jeu:
         self.__id_partie = id_partie
         self.__nb_tentatives = nb_tentatives
         self.__est_gagne = est_gagne
-        self.gestionnaire_score = Score() 
-        self.score = self.gestionnaire_score.get_valeur_score()
 
-        #je met les gets vue que c'est private 
-    def get_id_partie(self): 
+        self.gestionnaire_score = Score()
+        self.__score = 0  # score courant de la partie
+
+   
+    def get_id_partie(self):
         return self.__id_partie
-    def get_nb_tentatives(self): 
+
+    def get_nb_tentatives(self):
         return self.__nb_tentatives
-    def get_est_gagne(self): 
-        return self.__est_gagne  
-    def score(self): 
+
+    def get_est_gagne(self):
+        return self.__est_gagne
+
+    def get_score(self):
         return self.__score
 
-
-    def set_nb_tentatives(self, v:int):
+    
+    def set_nb_tentatives(self, v: int):
         self.__nb_tentatives = v
         return self.__nb_tentatives
-    
-    def set_est_gagne(self, v:bool):
+
+    def set_est_gagne(self, v: bool):
         self.__est_gagne = v
         return self.__est_gagne
 
-    
-    def terminer_partie(self, a_gagne, nb_tours_finaux):
-        self.est_gagne (a_gagne)
-        self.nb_tentatives (nb_tours_finaux)
-    
-        self.score = self.gestionnaire_score.calculer(nb_tours_finaux) if a_gagne else 0
+    def set_score(self, v: int):
+        self.__score = v
         return self.__score
-        
-            
+
+  
+    def terminer_partie(self, a_gagne: bool, nb_tours_finaux: int):
+        self.set_est_gagne(a_gagne)
+        self.set_nb_tentatives(nb_tours_finaux)
+        self.set_score(self.gestionnaire_score.calculer(nb_tours_finaux) if a_gagne else 0)
+        return self.__score
+
     def sauvegarder_partie(self):
         return {
             "id": self.get_id_partie(),
@@ -43,4 +49,3 @@ class Jeu:
             "tentatives": self.get_nb_tentatives(),
             "victoire": self.get_est_gagne()
         }
-        

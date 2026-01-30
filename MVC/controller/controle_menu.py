8dc4490cf262695d371c254562b15387fff0b215
Perrine Rousset <prousset01@pfm-ad.pfm.loc>
2026-01-30 15:55:41 +0100

    gestion du bug relance de partie

--- MVC/controller/controle_menu.py
+++ MVC/controller/controle_menu.py
@@ -18,7 +18,7 @@ class Controleur:
         self.gestionnaire_historique = historique()
         self.historique_parties = []
         self.compteur_id = 1
-        self.afficher_accueil()   
+        self.gestionnaire_historique = historique()   
         self.root.mainloop()
         
     def afficher_accueil(self):
@@ -28,15 +28,25 @@ class Controleur:
 
     def nouvelle_partie(self):
             if self.partie_objet:
-                self.gestionnaire_historique.ajouter_au_fichier(self.partie_objet)
-                self.historique_parties.append(self.partie_objet)
+                try:
+                    if hasattr(self, 'gestionnaire_historique'):
+                        self.gestionnaire_historique.ajouter_au_fichier(self.partie_objet)
+                    self.historique_parties.append(self.partie_objet)
+                except Exception as e:
+                    print(f"Note: Sauvegarde ignorée ou erreur : {e}")
+
             if self.vue_jeu_active:
+                try:
                     self.vue_jeu_active.frame_principale.destroy()
-                    self.vue_jeu_active = None
+                except:
+                    pass
+                self.vue_jeu_active = None
+
             self.vue_principale.nettoyer_zone_centrale()
             self.modele.Combinaison_secrete() 
             self.partie_objet = Jeu(id_partie=self.compteur_id)
             self.compteur_id += 1
+            from view.console import VueJeu
             self.vue_jeu_active = VueJeu(self.vue_principale.zone_centrale, self.modele, self)
                     
     def afficher_jeu(self):
